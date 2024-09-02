65d0af1418d47026581abc9ddf431ec522670b35
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2024-09-02 18:18:42 +0530

    ecommit

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const video_description = require('./Routes/video_description')
 const video_growth = require('./Routes/video_growth')
 const mp4_to_text = require('./Routes/mp4_to_text')
 const generate_pdf = require('./generate_pdf')
+const user = require('./Routes/users')
+
 
 
 app.use("/uploads", express.static("uploads"))
@@ -21,6 +23,7 @@ app.use('/video',video_description);
 app.use('/growth',video_growth);
 app.use('/',generate_pdf);
 app.use('/',mp4_to_text);
+app.use('/user',user);  
 app.get('/',(req,res)=>{
     res.json({msg:"Welcome, Welcome, Bhale Padhara"});
 })
