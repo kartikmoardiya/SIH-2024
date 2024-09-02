f6e96d11aeaee1c7531e4da5f8d692c72dd3c1f0
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2024-09-02 06:49:15 +0530

    ecommit

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const youtube_search = require('./Routes/youtube_search')
 const video_description = require('./Routes/video_description')
 const video_growth = require('./Routes/video_growth')
 const mp4_to_text = require('./Routes/mp4_to_text')
-const test = require('./test')
+const generate_pdf = require('./Routes/generate_pdf')
 
 
 app.use(bodyParser.json());
 app.use('/youtube',youtube_search);
 app.use('/video',video_description);
 app.use('/growth',video_growth);
-app.use('/test',test)
+app.use('/',generate_pdf);
 app.use('/',mp4_to_text);
 app.get('/',(req,res)=>{
     res.json({msg:"Welcome, Welcome, Bhale Padhara"});
